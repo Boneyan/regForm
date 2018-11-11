7baf03b2b56007c0021ef0c87f66802046d6ff80
Konnov Konstantin <boneyandoom@gmail.com>
2018-11-11 22:42:28 +0300

    test666

--- config.py
+++ config.py
@@ -14,5 +14,6 @@ basedir = os.path.abspath(os.path.dirname(__name__))
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-SQLALCHEMY_DATABASE_URI = 'postgres://lylymyqhigjzss:1c3b489f259e7b2c54e499815645815a23677a054f009024fde9fd9eab608bc4@ec2-50-19-127-158.compute-1.amazonaws.com:5432/dc07g7o33t2kt4'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.sqlite')
+
 DATABASE_URI = 'postgres://lylymyqhigjzss:1c3b489f259e7b2c54e499815645815a23677a054f009024fde9fd9eab608bc4@ec2-50-19-127-158.compute-1.amazonaws.com:5432/dc07g7o33t2kt4'
